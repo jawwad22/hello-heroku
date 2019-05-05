57af256671ea51dea4bb35ff89b048ef6f8a8702
unknown <mjawwadiqbal22@gmail.com>
2019-05-04 22:03:50 -0700

    -hhelo

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.set('port', process.env.PORT || 5000);
 var port = app.get('port');
 
 app.get('/', function (req, res) {
-    res.send('hello world')
+    res.send('hello world kiya haal hain')
 });
 app.listen(port, () => {
     console.log(`Server running ${port}`);
